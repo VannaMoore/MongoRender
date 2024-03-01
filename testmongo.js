730eff0ba504f2ba5de74967d8054b786680fa9a
VannaMoore <121504543+VannaMoore@users.noreply.github.com>
2024-03-01 00:23:05 +0000

    save changes

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mongodb+srv://notAdmin:notAdmin@mongonode.jeqkupw.mongodb.net/?retryWrites=true&w=majority&appName=MongoNode";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -35,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('Node_Mongo');
+    const parts = database.collection('Node_Mongo');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
